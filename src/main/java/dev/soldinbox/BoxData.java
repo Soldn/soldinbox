dde2a51f714a90f244688060ee7c61b99979d8e6
Soldn <kyryll524@gmail.com>
2025-08-26 11:08:33 +0100

    Update BoxData.java

--- src/main/java/dev/soldinbox/BoxData.java
+++ src/main/java/dev/soldinbox/BoxData.java
@@ -1,27 +1,27 @@
 package dev.soldinbox;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-
 import java.util.List;
 
 public class BoxData {
-    private final int id;
-    private Location location;
+    private int id;
     private List<LootEntry> loot;
 
-    public BoxData(int id, Location location, List<LootEntry> loot) {
+    public BoxData(int id, List<LootEntry> loot) {
         this.id = id;
-        this.location = location;
         this.loot = loot;
     }
 
-    public int getId() { return id; }
-    public Location getLocation() { return location; }
-    public void setLocation(Location location) { this.location = location; }
-    public List<LootEntry> getLoot() { return loot; }
-    public void setLoot(List<LootEntry> loot) { this.loot = loot; }
+    public List<LootEntry> getLoot() {
+        return loot;
+    }
 
-    // GUI holder marker
-    public static class EditorHolder {}
+    public void setLoot(List<LootEntry> loot) {
+        this.loot = loot;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static class EditorHolder {} // Заглушка для GUI
 }
